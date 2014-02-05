5193560c3d7eef754bb4cf6e87a9f8f08e919ce4
simons <simon.sarazin@gmail.com>
2014-02-05 19:47:16 +0100

    update settings

--- unisson/settings.py
+++ unisson/settings.py
@@ -18,7 +18,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'unisson.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'ATOMIC_REQUESTS': True,
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
