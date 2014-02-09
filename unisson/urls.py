df1c02d2dd07be5d21d5702ec9d3539dbacf8dc4
simons <simon.sarazin@gmail.com>
2014-02-09 01:33:04 +0100

    Updated urls.py
    not sure about configuration. Should be checked

--- unisson/urls.py
+++ unisson/urls.py
@@ -2,13 +2,25 @@ from django.conf.urls import include, patterns, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.generic import TemplateView
+
+from .views import RootRouter
 
 admin.autodiscover()
 
 urlpatterns = i18n_patterns('',
+	url(r'^$', RootRouter.as_view(), name='root'),
+    url(r'^home$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('zinnia.urls')),
 	url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^', include('social_auth.urls')),    
+    url(r'^', include('accounts.urls')),  
+)
+
+urlpatterns += i18n_patterns('',
     url(r'^', include('cms.urls')),
 )
 
