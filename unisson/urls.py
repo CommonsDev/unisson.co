9f61d4e50e4698a91cf616c31fa84388dce3f8a1
simons <simon.sarazin@gmail.com>
2014-02-09 02:56:35 +0100

    update template and url config
    Updated template waiting for social auth resolution.
    Changed autoredirection to homepage.

--- unisson/urls.py
+++ unisson/urls.py
@@ -11,7 +11,6 @@ from .views import RootRouter
 admin.autodiscover()
 
 urlpatterns = i18n_patterns('',
-	url(r'^$', RootRouter.as_view(), name='root'),
     url(r'^home$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('zinnia.urls')),
@@ -30,3 +29,5 @@ if settings.DEBUG:
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'', include('django.contrib.staticfiles.urls')),
 ) + urlpatterns
+
+
