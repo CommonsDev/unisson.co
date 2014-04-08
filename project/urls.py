3ecda67f8c1f3974ed1524f2ee441aa6244ccbe0
simons <simon.sarazin@gmail.com>
2014-04-08 18:31:54 +0200

    Add & to url

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^(?P<slug>[-\w]+)/$', views.ProjectDetailView.as_view(), name='project-detail'),
     url(r'^$', ProjectListView.as_view(), name='project-list'),                       
-    url(r'^practice', PracticeListView.as_view(), name='practice-list'),          
-    url(r'^usage', UsageListView.as_view(), name='usage-list'),                      
+    url(r'^practice$', PracticeListView.as_view(), name='practice-list'),          
+    url(r'^usage$', UsageListView.as_view(), name='usage-list'),                      
 )
