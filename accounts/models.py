d833a604df594688bb9bbf12dd5dd9e1e74b6294
Eliot Berriot <contact@eliotberriot.com>
2014-06-23 16:10:16 +0200

    Removed two functions 'create_profile' and 'set_guardian_permissions' that were not used

--- accounts/models.py
+++ accounts/models.py
@@ -33,34 +33,6 @@ class Profile(UserenaBaseProfile):
         return ('profile-detail', [self.user.id])
 
 
-
-def create_profile(user=None, profile=None, *args, **kwargs):
-    """
-    Create user profile if necessary
-    """
-    if profile:
-        return { 'profile': profile }
-    if not user:
-        return
-    return { 'profile': Profile.objects.get_or_create(user=user)[0] }
-
-
-def set_guardian_permissions(user=None, profile=None, *args, **kwargs):
-    """
-    Give the user permission to modify themselves
-    """
-    if not user or not user.is_authenticated():
-        return
-    if profile:
-        # Give permissions to view and change profile
-        for perm in ASSIGNED_PERMISSIONS['profile']:
-            assign(perm[0], user, profile)
-
-    # Give permissions to view and change itself
-    for perm in ASSIGNED_PERMISSIONS['user']:
-        assign(perm[0], user, user)
-
-
 # Fix for #14 , using http://stackoverflow.com/questions/12142195/django-userena-edit-profile-forbidden
 from django.dispatch import receiver
 from django.db.models.signals import post_save
